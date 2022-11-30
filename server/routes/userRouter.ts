842f7f784df98ee77c56e275489698d17df250aa
jsdeemon <jsdeemon@gmail.com>
2022-11-30 13:03:21 +0500

    3

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -4,14 +4,15 @@ const userController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
+
+// Private routes
+router.put('/:id', checkRoleMiddleware('ADMIN'), userController.updateUser)
+router.get('/auth', authMiddleware, userController.check)
+router.get('/admin', checkRoleMiddleware('ADMIN'), userController.check)
+
 // Public routes
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/:id', userController.getOneUser)
-
-// Private routes
-router.get('/auth', authMiddleware, userController.check)
-router.get('/admin', checkRoleMiddleware('ADMIN'), userController.check)
-router.put('/:id', checkRoleMiddleware('ADMIN'), userController.updateUser)
+router.get('/:id', userController.getOneUser)// 
 
 module.exports = router
